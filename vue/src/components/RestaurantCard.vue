576a331a2c28e237a055ddf8f231c5b8cb9a03ac
Daniel <djcohen42912gmail.com[D[D[D[D[D[D[D[D[D[D[D[C[@>
2022-08-09 13:57:28 -0400

    added API skeleton

---
+++ vue/src/components/RestaurantCard.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <p>(click name for more details)</p>
+    <h1>restaurant name</h1>
+    <img src="" alt="" /><p>image</p>
+    <p>tags</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "restaurant-card",
+    data(){
+        return{
+            restaurant:{
+                    id: "",
+                    name: "",
+                image_url:"",
+                is_closed: false,
+                url: "",
+                categories: [
+                    {
+                        alias: "",
+                        title: ""
+                    }
+                ],
+                rating: 0.0,
+                location: {
+                    address1: "",
+                    address2: null,
+                    address3: null,
+                    city: "",
+                    zip_code: "",
+                    country: "US",
+                    state: "",
+                    display_address: [
+                        "",
+                        ""
+                    ]
+                },
+                phone: "",
+                display_phone: "",
+                distance: "",
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
